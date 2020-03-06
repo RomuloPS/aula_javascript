bc824f6579f89d5879f97be2892d1f5532f26d85
Romulo Saraiva <romulopsaraiva@gmail.com>
2020-03-06 15:50:09 -0300

    Adding some console.log(s). Trying string function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 var nome = "Romulo Saraiva";
 var idade = 26;
-alert(nome + " tem " + idade + " anos");
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+
+//alert(idade+idade2);
+
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+
+//alert(frase.toUpperCase());
