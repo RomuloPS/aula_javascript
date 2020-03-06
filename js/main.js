f13641697a10aa532a2aeb1af3c97e07ebd7eb81
Romulo Saraiva <romulopsaraiva@gmail.com>
2020-03-06 15:52:16 -0300

    changing var name and name2 to n1 and n2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var nome = "Romulo Saraiva";
-var idade = 26;
-var idade2 = 10;
+var n1 = 26;
+var n2 = 10;
 var frase = "Japão é o melhor time do mundo"
-//alert(nome + " tem " + idade + " anos");
+//alert(nome + " tem " + n + " anos");
 
-//alert(idade+idade2);
+//alert(n+n2);
 
 console.log(nome);
-console.log(idade+idade2);
+console.log(n1+n2);
 console.log(frase.replace("Japão","Brasil"));
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
